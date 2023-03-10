f614e739df7a83f132c68e7c8598897eb68f2cdf
Adam-Barto <adam.jbarto@gmail.com>
2023-03-10 11:13:08 -0500

    Sets Name and Age

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,37 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
-
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        setName(name);
     }
 
     public Person(String name, int age) {
+        setAge(age);
+        setName(name);
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
